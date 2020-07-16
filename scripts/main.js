07d025f8d25ae8fbc90f9fd527ace02563d3a72f
DavidDelToro <68370316+DavidDelToro@users.noreply.github.com>
2020-07-16 12:53:17 +0300

    pic

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,18 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc !== 'images/firefox.jpg') {
-        myImage.setAttribute ('src','images/firefox.jpg');
-    } else {
-        myImage.setAttribute ('src','images/index.jpg');
+    switch (mySrc)
+    {
+        case 'images/firefox.jpg':
+            myImage.setAttribute ('src','images/index.jpg');
+            break;
+        case 'images/index.jpg':
+            myImage.setAttribute ('src','images/123index.jpg');
+            break;
+
+        case 'images/123index.jpg':
+            myImage.setAttribute ('src','images/firefox.jpg');
+            break;
     }
 }
 
